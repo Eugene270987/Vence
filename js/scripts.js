a9634d8c72a1dfc538fdda1e4773c98a41da480b
Eugene270987 <anikevich.it@gmail.com>
2023-06-28 11:58:22 +0300

    added some sections

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            1230:{
+                items: 2
+            }
+        }
+    });
+});
